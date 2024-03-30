3ae01a4446de81b99cb7d8697562812b0d9a8498
Min_h_bot <74346735+K-Minhyeok@users.noreply.github.com>
2024-03-31 01:13:54 +0900

    init:자 선수입장~

---
+++ src/test/java/org/example/canon/CanonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.canon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
