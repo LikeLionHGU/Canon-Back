3e9dcd8584dca298ec16464010d844bea0abeeb9
Min_h_bot <74346735+K-Minhyeok@users.noreply.github.com>
2024-04-16 13:21:11 +0900

    feat: Mapping 수정

--- src/main/java/org/example/canon/service/PostService.java
+++ src/main/java/org/example/canon/service/PostService.java
@@ -45,12 +45,7 @@ public class PostService {
     return posts.stream().map(PostDTO::of).toList();
   }
 
-  // 컨펌하는 로직
-  public void confirmPost(Long postId, AdminConfirmRequest request) {
-      Post post = postRepository.findById(postId).orElseThrow(PostNotFoundException::new);
-      post.confirmPost(request.getDecision());
-      postRepository.save(post);
-  }
+
 
   // 수정하는 로직
 
