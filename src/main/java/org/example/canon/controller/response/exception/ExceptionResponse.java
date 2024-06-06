fe612b2e48f4842fc1ba5aa7194ab96a803aad4e
K-Minhyeok <22000082@handong.ac.kr>
2024-06-06 18:23:05 +0900

    fix: 에러 출력

---
+++ src/main/java/org/example/canon/controller/response/exception/ExceptionResponse.java
@@ -0,0 +1,13 @@
+package org.example.canon.controller.response.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class ExceptionResponse {
+    private String error;
+    private String message;
+}
