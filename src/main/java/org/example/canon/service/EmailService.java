8b97edac6bc0c54e7443341c7f59ced76e1573f3
kai <lucas0606@handong.ac.kr>
2024-06-08 05:28:27 +0900

    feat : 이메일 추가

--- src/main/java/org/example/canon/service/EmailService.java
+++ src/main/java/org/example/canon/service/EmailService.java
@@ -53,7 +53,7 @@ public class EmailService {
                 "    <hr style=\"border: none; border-top: 2px solid #39007C; width: 80%; margin: 20px auto;\">\n" +
                 "    <p style=\"font-size: 18px; line-height: 1.5; margin-bottom: 20px;\">\n" +
                 "      안녕하세요 [ " + receiverName + " ]님,<br>\n" +
-                "      [HUP] 플랫폼을 통해 [ " + receiverName + " ]님께 커피챗 요청이 도착했습니다.<br>\n" +
+                "      [ HUP ] 플랫폼을 통해 [ " + receiverName + " ]님께 커피챗 요청이 도착했습니다.<br>\n" +
                 "      [ " + senderName + " ]님은 [ " + receiverName + " ]님의 졸업 작품에 큰 관심을 가지고 있습니다.<br>\n" +
                 "      또한 함께 졸업 작품에 대한 지식을 공유하고,<br>\n" +
                 "      창의적인 아이디어를 나누고 싶어합니다.<br>\n" +
@@ -65,8 +65,8 @@ public class EmailService {
                 "    <p style=\"font-size: 14px; color: #666; margin-bottom: 0;\">답장은 위 연락처로 부탁드립니다.</p>\n" +
                 "    <div style=\"margin-top: 20px; display: flex; justify-content: center;\">\n" +
                 "      <div style=\"display: flex; justify-content: center; flex-wrap: wrap;\">\n" +
-                "        <a href=\"#\" style=\"display: inline-block; background-color: #39007C; color: white; text-decoration: none; padding: 12px 24px; border-radius: 5px; margin: 10px; font-weight: bold; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); transition: all 0.3s ease;\">웹사이트</a>\n" +
-                "        <a href=\"#\" style=\"display: inline-block; background-color: #333; color: white; text-decoration: none; padding: 12px 24px; border-radius: 5px; margin: 10px; font-weight: bold; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); transition: all 0.3s ease;\">문의</a>\n" +
+                "        <a href=\"https://github.com/LikeLionHGU/DaePo_Front\" style=\"display: inline-block; background-color: #39007C; color: white; text-decoration: none; padding: 12px 24px; border-radius: 5px; margin: 10px; font-weight: bold; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); transition: all 0.3s ease;\">Front</a>" +
+                "        <a href=\"https://github.com/LikeLionHGU/Canon-Back\" style=\"display: inline-block; background-color: #333; color: white; text-decoration: none; padding: 12px 24px; border-radius: 5px; margin: 10px; font-weight: bold; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); transition: all 0.3s ease;\">Back</a>\n" +
                 "      </div>\n" +
                 "    </div>\n" +
                 "  </div>\n" +
